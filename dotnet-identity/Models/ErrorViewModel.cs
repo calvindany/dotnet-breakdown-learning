b49ed90dc6b4d907f33fd570e4d9c2bb9c84e681
calvindany <calvindany1102@gmail.com>
2025-01-08 09:28:37 +0700

    feat: dotnet-identity initial project

---
+++ dotnet-identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet_identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
